bbb83ef09059c7ac80a5e9545fdff0ca942a0e09
jantayg <70889084+jantayg@users.noreply.github.com>
2021-08-03 13:19:00 +0200

    files swapped from a different project as this one stopped working in gradle

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,22 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BaseTest {
+  WebDriver driver;
+
+  @BeforeEach
+  public void setup() {
+    WebDriverManager.chromedriver().setup();
+    this.driver = new ChromeDriver();
+  }
+
+  @AfterEach
+  public void tearDown() {
+    driver.close();
+  }
+}
